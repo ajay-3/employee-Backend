4963c18b04773947aa58ef64c1fd4d2ab463d164
ajay-3 <ajay.hn3@gmail.com>
2020-03-18 17:26:51 +0530

    edited

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,11 @@
 const express            = require('express');
 const cors               = require('cors');
-
+var mongoose             = require('mongoose');
 const bodyParser         = require('body-parser');
 const Employee           = require('./Data/employee.js');
+const Counter            = require('./Data/counter.js')
 const upload             = require("./services/file-upload");
 
-// const MongoClient = require("mongodb").MongoClient;
-// const ObjectId = require("mongodb").ObjectID;
-
-// const CONNECTION_URL = "mongodb+srv://<usr>:<psw>@employee-cfp3v.mongodb.net/test?retryWrites=true&w=majority";
-// const DATABASE_NAME = "Employee";
- 
  
   var app = express();
   
@@ -18,9 +13,7 @@ const upload             = require("./services/file-upload");
   app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
-  var database, collection;
   const singleUpload       = upload.single('image');
-  let prsnt_id = 0;
 
 app.post("/api/imageUpload", async (req, res)=> {
     singleUpload(req, res, (err)=> {
@@ -33,21 +26,27 @@ app.post("/api/imageUpload", async (req, res)=> {
 
 app.post("/api/addEmployee",async (req,res)=>{
       try{
-        console.log(req.body)
-        prsnt_id = prsnt_id + 1;
-        const employee = new Employee({
-          Id:prsnt_id,
-          Name:req.body.Name,
-          Salary:req.body.Salary,
-          DOB:req.body.DOB,
-          Skills:req.body.Skills,
-          ImageUrl:req.body.imageUrl  
-        });
-        const employeeProfile = await employee.save();
-        console.log(employeeProfile)
-        return res.json(employeeProfile);
+        var check = await Counter.find();
+       if(check.length!=0){
+         console.log("enter")
+        var brek= await Counter.update({Id:"uniqueKey"},{$inc: {count:1}});
+         console.log(brek) 
+      }else{
+        const counter = new Counter({Id:"uniqueKey",count:1});
+        await counter.save()
+       }
+       var result = await Counter.find();
+       const employee = new Employee({
+        Id:result[0].count,
+        Name:req.body.Name,
+        Salary:req.body.Salary,
+        DOB:req.body.DOB,
+        Skills:req.body.Skills,
+        ImageUrl:req.body.imageUrl
+      });
+      const employeeProfile = await employee.save();
+      return res.json(employeeProfile);
       }catch(err){
-        console.log(err)
         res.json({message:err})
       }
 })
@@ -64,7 +63,7 @@ app.get("/api/getEmployees",async (req,res)=>{
 app.post('/api/searchemployees', async (req, res)=>{
   try{
     var pattern = req.body.searchValue;
-  const matchEmployees = await	Employee.find({Name : {$regex: pattern }});
+  const matchEmployees = await	Employee.find({Name : {$regex: pattern}});
   res.json(matchEmployees);
 }catch(err){
   res.json({message:err})
@@ -72,9 +71,8 @@ app.post('/api/searchemployees', async (req, res)=>{
 }); 
 
 
-app.post("/api/updateEmployee",async (req,res)=>{
+app.put("/api/updateEmployee",async (req,res)=>{
     try{
-      console.log(req.body)
       var query = {
         Name:req.body.Name,
         Salary:req.body.Salary,
@@ -89,29 +87,21 @@ app.post("/api/updateEmployee",async (req,res)=>{
     }
 });
 
-app.post('/api/deleteEmployee', async (req, res)=>{
+app.delete('/api/deleteEmployee/:Id', async (req, res)=>{
   try{
-  const result=Employee.remove({Id:req.body.Id});
-  res.json(result);
+    console.log(req.params.Id)
+  const result= await Employee.remove({Id:req.params.Id});
+  res.status(200).json(result);
 }catch(err){
     res.json({message:err})
   }
 });
   
 
-// var url = '';
-// mongoose.connect(url,{useNewUrlParser:true});
-// var db = mongoose.connection;
-// db.on('connected',()=>{console.log("The connection has been established")})
-// db.on('error', (err)=>{console.log("There has been an error which is" +err)});
-// app.listen(app.get('port'),()=>{
-//   MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-//       if(error) {
-//           throw error;
-//       }
-//       database = client.db(DATABASE_NAME);
-//       collection = database.collection("employee");
-//       console.log("Connected to `" + DATABASE_NAME + "`!");
-//   });});
+var url = 'mongodb://127.0.0.1:27017/Employee';
+mongoose.connect(url,{useNewUrlParser:true});
+var db = mongoose.connection;
+db.on('connected',()=>{console.log("The connection has been established")})
+db.on('error', (err)=>{console.log("There has been an error which is" +err)});
 
-app.listen(app.get('port'),()=>{})
+app.listen(app.get('port'),()=>{console.log("Server started at "+app.get("port"))})
